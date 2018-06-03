7ce5bbe2840df84c22215468fbdda68309c8f8a3
Vitor Matheus <vitormatheussc99@gmail.com>
2018-06-03 15:17:06 -0400

    Remove cache files

--- models/database.py
+++ models/database.py
@@ -13,11 +13,19 @@ class DataBase:
     def kill(self):
         self.db.close()
 
+    def tryHeuristic(self, _word):
+        return True
+    
     def queryData(self, _word):
         cur = self.db.conn.cursor()
         
         _word = re.findall(r'[a-zéúíóáõãêûîôâç\-]*', _word.lower())[0]
         cur.execute("SELECT COUNT(1) FROM word WHERE wrd = '%s'" % _word)
-        # cur.execute("SELECT COUNT(1) FROM word WHERE LOWER(wrd) LIKE REGEXP_REPLACE(LOWER('%s'), '[^a-zéúíóáõãêûîôâç\-]*', '', 'g')" % _word)
         
-        return cur.fetchone()[0] != 0
+        if (cur.fetchone()[0] == 0):
+            return False
+            # return self.tryHeuristic(_word)
+        else:
+            return True 
+        
+    
